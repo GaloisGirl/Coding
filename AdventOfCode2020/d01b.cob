40459fe833c23637156ed5e2b8001ba9a2f22e3c
Anna Kosieradzka <a.wrochna@gmail.com>
2020-12-03 01:19:04 +0100

    Update d01b.cob

--- AdventOfCode2020/d01b.cob
+++ AdventOfCode2020/d01b.cob
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Advent-of-Code-2020-day-1.
+       PROGRAM-ID. Advent-of-Code-2020-day-1-part-2.
        AUTHOR. ANNA KOSIERADZKA.
       
        ENVIRONMENT DIVISION.
