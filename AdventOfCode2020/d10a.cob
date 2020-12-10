1093f6b42d1b05b178cbb645745a4c18390b9a0b
Anna Kosieradzka <a.wrochna@gmail.com>
2020-12-10 23:02:30 +0100

    AOC2020 day 10 part 1: code cleanup

--- AdventOfCode2020/d10a.cob
+++ AdventOfCode2020/d10a.cob
@@ -5,7 +5,7 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT INPUTFILE ASSIGN TO "d10a.input"
+           SELECT INPUTFILE ASSIGN TO "d10.input"
            ORGANIZATION IS LINE SEQUENTIAL.
            
        DATA DIVISION.
@@ -18,7 +18,7 @@
        WORKING-STORAGE SECTION.
          01 FILE-STATUS PIC 9 VALUE 0.
          01 REC-LEN PIC 9(2) COMP.
-         01 WS-ARR-LEN PIC 9(2) VALUE 13.
+         01 WS-ARR-LEN PIC 9(2) VALUE 95.
          01 WS-ARRAY OCCURS 11 TO 99 DEPENDING ON WS-ARR-LEN.
            05 WS-ARR-I PIC 9(3).
 
@@ -39,7 +39,6 @@
            SORT WS-ARRAY ON ASCENDING KEY WS-ARR-I.
            PERFORM 004-SHIFT-ARRAY.
            COMPUTE WS-ARR-I(WS-ARR-LEN) = WS-ARR-I(WS-ARR-LEN - 1) + 3.
-           DISPLAY WS-ARR-I(WS-ARR-LEN).
            PERFORM 005-USE-ADAPTERS.
            COMPUTE RESULT = DIFF-1 * DIFF-3.
            DISPLAY RESULT.
@@ -53,7 +52,6 @@
        
        003-PROCESS-RECORD.
            ADD 1 TO RESULT.
-           DISPLAY I "," INPUTRECORD.
            MOVE INPUTRECORD TO WS-ARR-I(I).
            ADD 1 TO I.
 
@@ -65,7 +63,6 @@
        005-USE-ADAPTERS.
            PERFORM VARYING I FROM 1 BY 1 UNTIL I > WS-ARR-LEN - 1
               COMPUTE DIFF = WS-ARR-I(I + 1) - WS-ARR-I(I)
-              DISPLAY DIFF
               IF DIFF = 1 THEN
                  ADD 1 TO DIFF-1
               END-IF
